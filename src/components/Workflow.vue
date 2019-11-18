dc74e7a570d4ba8ed77ee000fda437bd5e70fcbf
yingfeng-iu <yingfeng@iu.edu>
2019-11-18 11:07:29 -0500

    AMP-435: add logic for enable/disable submit button

--- src/components/Workflow.vue
+++ src/components/Workflow.vue
@@ -21,7 +21,7 @@
             </div>
           </div>
           <div class="workflow-submit">
-              <input v-on:click="submit" type="button" value="Submit" class="primary-button">
+              <input v-on:click="submit" type="button" value="Submit" :disabled="!(selectedWorkflow && files)" class="primary-button">
           </div>
         </div>
       </div>
