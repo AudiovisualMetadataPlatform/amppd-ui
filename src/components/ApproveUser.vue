9b983d667fe10b9d631250c5fdfb1f2f6946505c
vinitab <vinitab@vinita-dell>
2020-04-27 17:40:04 -0400

    AMP-651_rejectUserUI

--- src/components/ApproveUser.vue
+++ src/components/ApproveUser.vue
@@ -1,27 +1,39 @@
 <template>
-  <div>
+  <div class="main-body">
     <Header/>
     <div class="form-body">
-      <h1>Approve User</h1>
-      <div class="error">
-        <p v-if="errors.length">
-          <b>Please correct the following error(s):</b>
-          <ul>
+      <h1>Welcome to the Audiovisual Metadata Platform</h1>
+      <div class="form-content" id="login">
+        <h2>Approve User</h2>
+        <form>
+          <div class="error">
+            <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
             <li v-for="error in errors" v-bind:key="error"><span>{{ error }}</span></li>
-          </ul>
-        </p>
-        </div>
-        <div class="form-content" id="approve">
-			<div class= "row"> User ID : {{this.$route.params.id}}</div>
-			<div class="row">Click <span><a href="#" v-on:click="approveUser()">here</a></span> to approve</div>
-			<div v-if="approve_user">
-				<label>The user has been approved. </label>
-			</div>
-        </div>
-    
-	</div>
+            </ul>
+            </p>
+          </div>
+          <div class="form-group">
+            <label for="email">User ID: {{this.$route.params.id}}</label>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary marg-bot-4" v-on:click="approveUser()">Approve</button>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary marg-bot-4" v-on:click="rejectUser()">Reject</button>
+          </div>
+          <div class="form-group" v-if="approve_user">
+            <label>The user has been approved. </label>
+          </div>
+          <div class="form-group" v-if="reject_user">
+            <label>The user has been rejected successfully. </label>
+          </div>
+        </form>
+      </div>  
+    </div>
   </div>
-</template>
+</template>  
 
 <script>
 import Header from '@/components/Header.vue';
@@ -36,32 +48,61 @@ import axios from 'axios';
 			errors: [],
 			id: null,
 			approve_status: false,
-			approve_user: false
+      approve_user: false,
+      reject_user: false
     }
   },
   methods:{
 	async approveUser() {
-		event.preventDefault();
-		await axios.post(process.env.VUE_APP_AMP_URL+ '/approve-user',
-          {
-            userId: this.$route.params.id 
-          })
-        .then(response => {
-          self.approve_status = response.data.success;
-        })
-        .catch(e => {
-          console.log(e);
+    event.preventDefault();
+    this.errors=[];
+    this.approve_user = false;
+    this.reject_user=false;
+		await axios.post(process.env.VUE_APP_AMP_URL+ '/user/account/approve',
+    {
+      userId: this.$route.params.id 
+    })
+    .then(response => {
+    self.approve_status = response.data.success;
+    })
+    .catch(e => {
+    console.log(e);
 		});
 		console.log("approve result is:"+self.approve_status);
 		if(this.errors.length == 0 && self.approve_status)
-        {
-			this.approve_user = true;
-        }
-        else
-        {
-          this.errors.push('User was not approved');
-        }
+    {
+      this.approve_user = true;
     }
+    else
+    {
+      this.errors.push('User was not approved');
+    }
+  },
+    async rejectUser() {
+    event.preventDefault();
+    this.errors=[];
+    this.approve_user = false;
+    this.reject_user=false;
+		await axios.post(process.env.VUE_APP_AMP_URL+ '/user/account/reject',
+    {
+      userId: this.$route.params.id 
+    })
+    .then(response => {
+      self.approve_status = response.data.success;
+    })
+    .catch(e => {
+      console.log(e);
+		});
+		console.log("reject result is:"+self.approve_status);
+		if(this.errors.length == 0 && self.approve_status)
+    {
+      this.reject_user = true;
+    }
+    else
+    {
+      this.errors.push('User was not approved');
+    }
+  }
   },
   mounted() {
     //console.log("IT WORKS");
@@ -70,77 +111,198 @@ import axios from 'axios';
 </script>
 
 <style scoped>
+
  .form-body{
-  margin-block-start: 100px;
-  width: auto;
+  max-width: 960px;
   height: auto;
   text-align: center;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: 9em;
+  margin-bottom: 2em;
+  font-size: 1rem;
+  text-align: left;
+  line-height: 1.5;
+  }
+
+  .forgot-psw {
+    font-size: 0.9rem;
+    margin-top: 0!important;
+
   }
 
   h1 {
-  text-align: center;
+  text-align: center!important;
+  font-size: 2.5rem;
+  margin-bottom: .5rem;
+  font-family: inherit;
+  font-weight: 500;
+  line-height: 1.2;
+  color: #000;
+
   }
+
+  h2 {
+    margin-bottom: .50rem;
+    font-size: 2rem;
+    font-weight: 500;
+    line-height: 1.2;
+    margin-top: 0;
+    display: block;
+    margin-block-start: 0.83em;
+    margin-block-end: 0.83em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    color: #000;
+  }
+
+  btn:not(:disabled):not(.disabled) {
+    cursor: pointer;
+}
+
+.btn-primary {
+    background: #F4871E;
+    border-color: #F4871E;
+    color: #153c4d;
+}
+
+.marg-bot-4 {
+    margin-bottom: 2rem;
+}
+
+.btn {
+    display: inline-block;
+    font-weight: 400;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    user-select: none;
+    border: 1px solid transparent;
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: .25rem;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+}
 
   .form-content{
-  /*padding-top:50px;*/
-    border-radius: 25px;
-    border: 1px solid;
-    padding: 20px 20px;
-    width: 50%;
+    margin-top: 2.5rem;
+    margin-right: auto;
+    margin-bottom: 2.5rem;
+    margin-left: auto;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-clip: border-box;
+    border: 1px solid rgba(0,0,0,.125);
+    border-radius: .25rem;
+    flex: 0 0 50%;
+    flex-grow: 0;
+    flex-shrink: 0;
+    flex-basis: 50%;
+    max-width: 60%;
+    width: 100%;
+    min-height: 1px;
+    padding-right: 25px;
+    padding-left: 25px;
+    text-align: left;
+  }
+
+  .text-muted {
+    color: #6c757d!important;
+  }
+
+  .form-group {
+    margin-bottom: 1rem;
+  }
+
+  label {
     display: inline-block;
+    margin-bottom: .5rem;
+    cursor: default;
+    width: 100%;
   }
 
-  input[type=text], input[type=password] {
-    border-radius: 5px;
-    padding: 15px 20px;
-    margin: 20px 20px;
-    border: 1px solid #ccc;
-    align-content: center;
-    width: 50%;
+.form-control {
+    display: block;
+    width: 100%;
+    height: calc(2.25rem + 2px);
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: .25rem;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+}
+
+button, input {
+    overflow: visible;
+    margin: 0;
+    font-family: inherit;
+    
+}
+button:hover {
+    color: #F4871E;
+    background-color: #153c4d;
   }
 
-  button {
-    background-color: #2C5B7F;
-    color: #E9972D;
-    font:bolder;
-    padding: 14px 20px;
-    margin-left: 20px;
-    margin-right: 20px;
-    margin-top: 20px;
-    margin-bottom: 20px;
-    border: none;
-    cursor: pointer;
-    border-radius: 15px;
-  }
+.active-link {
+  color: #153c4d;
+}
 
-  /* Add a hover effect for buttons */
-  button:hover {
-    opacity: 0.8;
-  }
+.active-link:hover {
+  color: #F4871E;
+}
 
-  .error {
-    padding: 20px 100px;  
+input {
+  text-rendering: auto;
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  text-align: start;
+  cursor: text;
+  font: 400 13.3333px Arial;
+}
+
+  .form-errors {
     color: red;
-  }
-
-  ul{
-  list-style: none;
-  } 
-
-  li span {
-    width : 70%;
-    padding-left: 100px;
-    padding-right: 100px;
+    margin: 0%!important;
+    font-size: 0.9rem;
   }
  
   p {
     width:70%;
-    text-align: center;
-    margin:auto;
+    text-align: left;
+    margin-bottom: 1rem;
+    /* margin:auto; */
   }
 
-  .row {
-    text-align: center;
-    padding:5px 10px;
-  }
+  *, ::after, ::before {
+    box-sizing: border-box;
+}
+
+ .main-body {
+  background: #fff;
+  height: 100%;
+  min-height: 100%;
+  margin: 0;
+  background-color: #fff;
+  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #404040;
+  text-align: left;
+  display: block;
+ }
+  
 </style>
