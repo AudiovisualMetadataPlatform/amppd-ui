d4ee8f02db0b554b7982447a0d326a5f26ebc733
vinitab <vinitab@vinita-dell>
2020-07-07 18:05:59 -0400

    AMP-728_setMaxDate

--- src/components/dashboard/DashboardFilters/DateFilter.vue
+++ src/components/dashboard/DashboardFilters/DateFilter.vue
@@ -8,7 +8,7 @@
 		<div class="form-group row">
 		<label for="colFormLabelFrom" class="col-sm-4 col-form-label col-form-label-sm text-right">From</label>
 		<div class="col-sm-8">
-			<input type="date" class="form-control form-control-sm" id="colFormLabelFrom" v-model="fromDate">
+			<input type="date" :max="getMaxDate()" class="form-control form-control-sm" id="colFormLabelFrom" v-model="fromDate">
 		</div>
 		</div>
 		<div class="form-group row">
@@ -51,6 +51,33 @@ export default {
 		self.workflowDashboard.searchQuery.filterByDates.push(new Date(self.fromDate.replace("-", '/')));
 		self.workflowDashboard.searchQuery.filterByDates.push(new Date(self.toDate.replace("-", '/')));
 		self.visible=false;
+	},
+	getMaxDate(){
+		console.log("inside getMaxDate()");
+		/* var dt = new Date();
+		var month = dt.getMonth() + 1;
+    	var day = dt.getDate();
+    	var year = dt.getFullYear();
+		if(month < 10)
+			month = '0' + month.toString();
+		if(day < 10)
+			day = '0' + day.toString();
+		//var maxDate = year + '-' + month + '-' + day; 
+		var maxDate = moment(new Date()).format('YYYY-MM-DD');
+		console.log("todays date is:"+maxDate); */
+		let today = new Date(),
+    	day = today.getDate(),
+    	month = today.getMonth()+1, //January is 0
+    	year = today.getFullYear();
+        if(day<10){
+                day='0'+day
+            } 
+        if(month<10){
+            month='0'+month
+        }
+		today = year+'-'+month+'-'+day;
+		console.log("todays date :"+today);
+		return today;
 	}
   },
 }
