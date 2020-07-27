4a82ad365085d995443b15d724ccc4c7e1921395
vinitab <vinitab@vinita-dell>
2020-07-27 18:28:10 -0400

    AMP-656_mediaInfoFixForIcon

--- src/components/workflow/SelectFilesLeft.vue
+++ src/components/workflow/SelectFilesLeft.vue
@@ -49,7 +49,7 @@
    </form>
    <div v-if="searchResults">
       <h3>Search Results</h3>
-      <div id="accordion" >
+      <div id="accordion" v-if="searchedItems.rows.length>0">
 
          <div class="card" v-for="(item, index) in searchedItems.rows" v-bind:key="index" >
             <div class="card-header" id="headingTwo">
@@ -78,14 +78,14 @@
                <div class="card-body">
                   <!-- -->
                   <ul class="list-unstyled file-list" >
-                     <li v-for="(filename, key, file_index) in item.primaryFiles" v-bind:key="file_index">
-                        <button class="btn btn-light btn-sm" :disabled=hasValue(key)>
+                     <li v-for="(filename, file_index) in item.primaryFiles" v-bind:key="file_index">
+                        <button class="btn btn-light btn-sm" :disabled=hasValue(filename.id)>
                            <svg class="icon-play  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
                               <path class="icon-play" d="M25.7 8.8c2.7 0 5.3 0 7.8 0.1C35.9 8.9 37.8 9 39 9.1l1.8 0.1c0 0 0.2 0 0.4 0s0.4 0 0.6 0.1c0.1 0 0.3 0.1 0.6 0.1 0.3 0 0.5 0.1 0.7 0.2 0.2 0.1 0.4 0.2 0.7 0.3 0.3 0.1 0.5 0.3 0.7 0.5 0.2 0.2 0.5 0.4 0.7 0.6 0.1 0.1 0.2 0.2 0.4 0.4 0.2 0.2 0.4 0.7 0.7 1.4 0.3 0.7 0.5 1.5 0.6 2.4 0.1 1 0.2 2.1 0.3 3.3 0.1 1.2 0.1 2.1 0.1 2.7v1 3.3c0 2.3-0.1 4.6-0.4 7 -0.1 0.9-0.3 1.7-0.6 2.4s-0.5 1.2-0.8 1.5L45 36.7c-0.2 0.2-0.5 0.5-0.7 0.6 -0.2 0.2-0.5 0.3-0.7 0.5s-0.5 0.2-0.7 0.3c-0.2 0.1-0.4 0.1-0.7 0.2 -0.3 0-0.5 0.1-0.6 0.1 -0.1 0-0.3 0-0.6 0.1 -0.2 0-0.4 0-0.4 0 -4 0.3-9 0.5-15 0.5 -3.3 0-6.2-0.1-8.6-0.2 -2.4-0.1-4-0.1-4.8-0.2L11 38.6l-0.9-0.1c-0.6-0.1-1-0.2-1.3-0.2 -0.3-0.1-0.7-0.2-1.2-0.5s-1-0.6-1.4-1c-0.1-0.1-0.2-0.2-0.4-0.4 -0.2-0.2-0.4-0.7-0.7-1.4s-0.5-1.5-0.6-2.4c-0.1-1-0.2-2.1-0.3-3.3 -0.1-1.2-0.1-2.1-0.1-2.7v-1 -3.3c0-2.3 0.1-4.6 0.4-7 0.1-0.9 0.3-1.7 0.6-2.4s0.5-1.2 0.8-1.5L6.3 11c0.2-0.2 0.5-0.5 0.7-0.6 0.2-0.2 0.5-0.3 0.7-0.5C8 9.8 8.2 9.7 8.4 9.6s0.4-0.1 0.7-0.2c0.3 0 0.5-0.1 0.6-0.1 0.1 0 0.3 0 0.6-0.1s0.4 0 0.4 0C14.6 8.9 19.6 8.8 25.7 8.8zM21.2 29.4l11.6-6 -11.6-6.1V29.4z"></path>
                            </svg>
-                           {{filename}}
+                           {{filename.name}}
                         </button>
-                        <button class="btn btn-link  add-remove float-right" v-on:click="addFiles(index,key)" v-bind:disabled=hasValue(key)>
+                        <button class="btn btn-link  add-remove float-right" v-on:click="addFiles(index,file_index)" v-bind:disabled=hasValue(item.primaryFiles[file_index].id)>
                            <svg class="icon-plus" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 311.5 311.5" style="enable-background:new 0 0 311.5 311.5;" xml:space="preserve">
                               <path class="circle-stroke" d="M156.8,302c-80.6,0-146.2-65.6-146.2-146.2S76.2,9.6,156.8,9.6S303,75.2,303,155.8S237.4,302,156.8,302z
                                  M156.8,27.9c-70.5,0-127.9,57.4-127.9,127.9s57.4,127.9,127.9,127.9s127.9-57.4,127.9-127.9S227.3,27.9,156.8,27.9z"></path>
@@ -101,6 +101,9 @@
             </div>
          </div>
 
+      </div>
+      <div v-else>  
+         No Results
       </div>
    </div>
 </div>
@@ -161,16 +164,16 @@ export default {
             console.log("the files are:"+self.searchedItems.length+" media_type is:"+media_type+" "+self.searchResults);
          }
       },
-		addFiles(index, key) {
+		addFiles(index, file_index) {
          let self = this;
-         if(!self.hasValue(key)){
-            self.selectedFiles.push({id:key,name:self.searchedItems.rows[index].primaryFiles[key]});
+         if(!self.hasValue(self.searchedItems.rows[index].primaryFiles[file_index].id)){
+            self.selectedFiles.push(self.searchedItems.rows[index].primaryFiles[file_index]);
          } 
          console.log("The file name selected is:",self.selectedFiles);
 		},
       addAllFiles(index){
          let self = this;
-         for(var key in self.searchedItems.rows[index].primaryFiles){
+         for(var key=0; key<self.searchedItems.rows[index].primaryFiles.length;key++){
             self.addFiles(index, key);
          }
       },
@@ -185,26 +188,17 @@ export default {
       hasValues(index){
          let self = this;
          var result = true;
-         for(var key in self.searchedItems.rows[index].primaryFiles){
-            if(!self.hasValue(key))
+         for(var key=0; key<self.searchedItems.rows[index].primaryFiles.length;key++){
+            if(!self.hasValue(self.searchedItems.rows[index].primaryFiles[key].id))
             {
                result = false;
                break;
             }
          }
+         console.log("result is:"+result);
          return result;
        },
-      //TODO: this function needs to be finished by assigning values to a temp variable
-      filterContent(index){
-         let self = this;
-         if(this.searchAudio){
-            for(var key in self.searchedItems.rows[index].primaryFiles){
-               if(self.searchedItems.rows[index].primaryFiles[key].ext != 'mp3'){
-                  self.searchedItems.rows[index].primaryFiles.delete[key];
-               }
-         }
-      }
-   }
+
    },
    watch:{
       searchAudio: function(){
