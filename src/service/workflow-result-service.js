169b6f9e9e0cd31b8e8a25a5a8e1edaafa1c67b8
dfische3 <dan@portalmedia.com>
2020-10-29 10:50:45 -0500

    Changing URLs to match dashboard renaming

--- src/service/workflow-result-service.js
+++ src/service/workflow-result-service.js
@@ -2,19 +2,17 @@
 import BaseService from './base-service'
 export default class WorkflowResultService extends BaseService{
     async getWorkflowResults(searchQuery){
-        var data = await super.post_auth('/dashboard',  searchQuery).
+        var data = await super.post_auth('/workflow-results',  searchQuery).
         then(result=>{
             return result.data;
         });
-        console.log(data);
         return data;
     }
     async setIsFinal(id, isFinal){
-        var data = await super.post_auth('/dashboard/isfinal/' + id + '?isFinal=' + isFinal,  null).
+        var data = await super.post_auth('/workflow-results/isfinal/' + id + '?isFinal=' + isFinal,  null).
         then(result=>{
             return result.data;
         });
-        console.log(data);
         return data;
     }
 }
