74d63775864f9812eff7a032b683df9134e67b4e
yingfeng-iu <yingfeng@iu.edu>
2021-10-20 16:10:36 -0400

    AMP-1444 remove .env.test and env.staging

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -1,23 +1,23 @@
 export const env = {
   getEnv(name) {
-	  return process.env[name]	
-//	  return this.$config[name] || process.env[name]	
+	  return this.$config[name] || process.env[name]	
+	  // return process.env[name]	
   },
 
   getDisableAuth() {
-	return this.getEnv('VUE_APP_DISABLE_AUTH')
+	  return this.getEnv('VUE_APP_DISABLE_AUTH')
   },
 
   getAmpUrl() {
-	return this.getEnv('VUE_APP_AMP_URL')
+	  return this.getEnv('VUE_APP_AMP_URL')
   },
 
   getAmpUnit() {
-	return this.getEnv('VUE_APP_AMP_UNIT')
+	  return this.getEnv('VUE_APP_AMP_UNIT')
   },
 
   getGalaxyWorkflowUrl() {
-	return this.getEnv('VUE_APP_GALAXY_WORKFLOW_URL')
+	  return this.getEnv('VUE_APP_GALAXY_WORKFLOW_URL')
   },
 
 }
