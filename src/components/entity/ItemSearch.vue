38ede7b2f3baa0339d04caa305332e017b55dae8
Arindam Ghosh <ghoshar@iu.edu>
2022-07-25 22:07:46 +0530

    - Search Item page loading issue fix

--- src/components/entity/ItemSearch.vue
+++ src/components/entity/ItemSearch.vue
@@ -64,29 +64,31 @@ export default {
     async refreshData() {
       const self = this;
       try {
-        self.loading= true;
-        self.itemSource = await this.workflowResultService.getWorkflowResults(
-          {filterOnly: true, filterByRelevant: true}
-        );
-        self.searchSource = await self.itemSource.filters['items']
+        self.loading = true;
+        self.itemSource = await this.workflowResultService.getWorkflowResults({
+          filterOnly: true,
+          filterByRelevant: true,
+        });
+        self.searchSource = await self.itemSource.filters["items"];
+        self.loading = false;
       } catch (error) {
-        console.log(error)
-        self.loading=false;
+        console.log(error);
+        self.loading = false;
       }
     },
   },
-  updated(){
+  updated() {
     this.onSearch();
   },
-  mounted(){
-    this.refreshData()
-  }
+  mounted() {
+    this.refreshData();
+  },
 };
 </script>
 
 <style lang="css">
 @import "/amppd-ui/src/styles/style.css";
-.item-search{
+.item-search {
   width: 100%;
 }
 </style>
