cf1a9427b336306fe7c6f1c2a22067f46fc42100
Arindam Ghosh <ghoshar@iu.edu>
2023-01-07 13:11:00 +0530

    [AMP-2387] Implement Review Outputs view

--- src/components/evaluation/TestResultsVisualiz.vue
+++ src/components/evaluation/TestResultsVisualiz.vue
@@ -294,46 +294,6 @@
                                       </button> -->
                                     </sortable-header>
                                   </tr>
-                                  <!-- <tr role="row">
-                                    <th
-                                      data-sortable="true"
-                                      data-field="file"
-                                      class="sorting slim-col-2"
-                                      tabindex="0"
-                                      aria-controls="myTable-2"
-                                      rowspan="1"
-                                      colspan="1"
-                                      aria-label="Unit: activate to sort column ascending"
-                                      style="width: 163.688px"
-                                      v-for="header in JSON.parse(
-                                        selectedTestResult.scores
-                                      ).headers"
-                                      :key="header.field"
-                                    >
-                                      {{ header.label }}
-                                      <button
-                                        type="button"
-                                        class="btn btn-link float-right data-pop"
-                                        data-title="Filter GT Start"
-                                        data-container="body"
-                                        data-toggle="popover"
-                                        data-placement="top"
-                                      >
-                                        <svg
-                                          xmlns="http://www.w3.org/2000/svg"
-                                          width="16"
-                                          height="16"
-                                          fill="currentColor"
-                                          class="bi bi-funnel-fill"
-                                          viewBox="0 0 16 16"
-                                        >
-                                          <path
-                                            d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2z"
-                                          />
-                                        </svg>
-                                      </button>
-                                    </th>
-                                  </tr> -->
                                 </thead>
                                 <tbody>
                                   <tr
