626700638a1876c2bbefe74fd5671b9855c9302e
yingfeng <yingfeng@iu.edu>
2024-10-29 18:03:53 -0400

    AMP-3426: remove commented code

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  // plugins: [require('unplugin-vue-components/webpack').default({ /* options */ })]
   // plugins: ["vuera/babel"]
 };
