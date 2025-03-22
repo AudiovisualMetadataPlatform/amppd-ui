16c6456c0e366db2050ad7114516f27afd4f565b
yingfeng <yingfeng@iu.edu>
2025-03-21 23:53:25 -0400

    AMP-3632/AMP-3638/AMP-3639/AMP-3640 - fix the reloading issue upon collection/item/file creation/update - add missing UI feedback upon success/failure of update - improve code structure - improve logs - remove commented code

--- src/service/entity-service.js
+++ src/service/entity-service.js
@@ -20,88 +20,90 @@ export default class EntityService {
 
 
     async onUpdateEntityDetails(self) {
-        if (self.baseUrl === 'unit') {
-        
+        self.submitted = true; 
+
+        if (self.baseUrl === 'unit') {   
             // unit Validation rules
             if (!self.entity.name || !self.entity.taskManager) {
                 self.$toast.error("Please provide required fields!", self.sharedService.toastNotificationConfig);
                 return false;
-            }
+            }     
             self.unitService.updateUnitDetails(self.selectedUnit.id, self.entity).then(response => {
-                self.isDataChanged = false;
-                self.$toast.success("Unit details updated successfully.", self.sharedService.toastNotificationConfig);
-            });
-        } else if (self.baseUrl === 'collection') {
-            self.submitted = true;
-            
-            // Collection Validation rules
-            if (!self.entity.name) {
-                self.$toast.error("Please provide required fields!", self.sharedService.toastNotificationConfig);
-                return false;
-            }
-            if (!self.isCreatePage) {
-                self.collectionService.updateCollection(self.entity).then(response => {
-                    self.$toast.success("Collection details updated successfully", self.sharedService.toastNotificationConfig);
-                    self.submitted = false;
-                    self.isDataChanged = false;
-                }).catch(error => {
-                    self.submitted = false;
-                    if (error.response && error.response.data && error.response.data.validationErrors) {
-                        const errorMessages = self.sharedService.extractErrorMessage(error.response.data.validationErrors);
-                        errorMessages.map(el => self.$toast.error(el, self.sharedService.toastNotificationConfig));
-                    } else {
-                        self.$toast.error("Unable to update a collection details. Please try again!!", self.sharedService.toastNotificationConfig);
-                    }
-                });
-            } else {
-                self.entity.unit = self.baseService.API_URL + `/units/${self.selectedUnit.id}`;
-                self.collectionService.createCollection(self.entity).then(response => {
-                    self.$toast.success("Collection created successfully", self.sharedService.toastNotificationConfig);
-                    self.entity = response;
-                    self.selectedCollection = response;
-                    self.selectedCollection.selectedCollectionId = response.id;                    
-                    self.submitted = false;
-                    self.isDataChanged = false;
-                    console.log("collection.id = " + self.entity.id);
-                    console.log("selectedCollectionId = " + self.selectedCollection.selectedCollectionId);
-                }).catch(error => {
-                    self.submitted = false;
-                    if (error.response && error.response.data && error.response.data.validationErrors) {
-                        const errorMessages = self.sharedService.extractErrorMessage(error.response.data.validationErrors);
-                        errorMessages.map(el => self.$toast.error(el, self.sharedService.toastNotificationConfig));
-                    } else {
-                        self.$toast.error("Unable to create a collection details. Please try again!!", self.sharedService.toastNotificationConfig);
-                    }
-                });
-            }
-        } else if (self.baseUrl === 'file') {
-            self.submitted = true;
-            
-            // file Validation rules
-            if (!self.entity.name) {
-                self.$toast.error("Please provide required fields!", self.sharedService.toastNotificationConfig);
-                return false;
-            }
-            self.showLoader = true;
-            const payload = { name: self.entity.name, description: self.entity.description };
-            self.primaryFileService.updatePrimaryFile(self.entity.id, payload).then(response => {
-                self.showLoader = false;
                 self.submitted = false;
-                self.$toast.success("File details has been updated successfully", self.sharedService.toastNotificationConfig);
                 self.isDataChanged = false;
+                console.log("Updated unit " + self.entity.id);
+                self.$toast.success("Unit details updated successfully.", self.sharedService.toastNotificationConfig);
             }).catch(error => {
-                self.showLoader = false;
                 self.submitted = false;
                 if (error.response && error.response.data && error.response.data.validationErrors) {
                     const errorMessages = self.sharedService.extractErrorMessage(error.response.data.validationErrors);
                     errorMessages.map(el => self.$toast.error(el, self.sharedService.toastNotificationConfig));
                 } else {
-                    self.$toast.error("File details update failed!", self.sharedService.toastNotificationConfig);
+                    self.$toast.error("Unable to update unit details. Please try again!", self.sharedService.toastNotificationConfig);
                 }
-            });
+            });      
+        } else if (self.baseUrl === 'collection') {           
+            // Collection Validation rules
+            if (!self.entity.name) {
+                self.$toast.error("Please provide required fields!", self.sharedService.toastNotificationConfig);
+                return false;
+            }
+            self.showLoader = true;
+            if (self.isCreatePage) {
+                self.entity.unit = self.baseService.API_URL + `/units/${self.selectedUnit.id}`;
+                self.collectionService.createCollection(self.entity).then(response => {
+                    self.entity = response;
+                    self.selectedCollection = response;
+                    self.selectedCollection.selectedCollectionId = response.id;                    
+                    self.showLoader = false;
+                    self.submitted = false;
+                    self.isDataChanged = false;
+                    // // TODO below is a workaround but doesn't work
+                    // // save selectedCollection into localStorage as sync doesn't seem to take effect on write
+                    // let vuex = JSON.parse(localStorage.getItem("vuex"));                    
+                    // localStorage.setItem("vuex", JSON.stringify({
+                    //     ...vuex,
+                    //     selectedCollection: self.selectedCollection,
+                    // }));                       
+                    // console.log("Created collection " + self.entity.id);
+                    self.$toast.success("Collection created successfully.", self.sharedService.toastNotificationConfig);
+                    self.$router.push("/collection/details");
+                }).catch(error => {
+                    self.showLoader = false;
+                    self.submitted = false;
+                    if (error.response && error.response.data && error.response.data.validationErrors) {
+                        const errorMessages = self.sharedService.extractErrorMessage(error.response.data.validationErrors);
+                        errorMessages.map(el => self.$toast.error(el, self.sharedService.toastNotificationConfig));
+                    } else {
+                        self.$toast.error("Unable to create collection. Please try again!", self.sharedService.toastNotificationConfig);
+                    }
+                });
+            } else {
+                self.collectionService.updateCollection(self.entity).then(response => {
+                    self.showLoader = false;
+                    self.submitted = false;
+                    self.isDataChanged = false;
+                    // TODO below is a workaround:
+                    // save selectedCollection into localStorage as sync doesn't seem to take effect on write
+                    let vuex = JSON.parse(localStorage.getItem("vuex"));                    
+                    localStorage.setItem("vuex", JSON.stringify({
+                        ...vuex,
+                        selectedCollection: self.selectedCollection,
+                    }));                    
+                    console.log("Updated collection " + self.entity.id);
+                    self.$toast.success("Collection details updated successfully.", self.sharedService.toastNotificationConfig);
+                }).catch(error => {
+                    self.showLoader = false;
+                    self.submitted = false;
+                    if (error.response && error.response.data && error.response.data.validationErrors) {
+                        const errorMessages = self.sharedService.extractErrorMessage(error.response.data.validationErrors);
+                        errorMessages.map(el => self.$toast.error(el, self.sharedService.toastNotificationConfig));
+                    } else {
+                        self.$toast.error("Unable to update collection details. Please try again!", self.sharedService.toastNotificationConfig);
+                    }
+                });
+            }
         } else if (self.baseUrl === 'item') {
-            self.submitted = true;
-
             // item Validation rules
             if (!self.entity.name) {
                 self.$toast.error("Please provide required fields!", self.sharedService.toastNotificationConfig);
@@ -114,12 +116,14 @@ export default class EntityService {
                     collection: env.getAmpUrl() + `/collections/${self.selectedCollection.id}`
                 }
                 self.itemService.addItemToCollection(self.entity).then(response => {
-                    self.showLoader = false;
-                    self.submitted = false;
                     self.entity = response;
                     self.selectedItem = response;
                     self.selectedItem.selectedItemId = response.id;
+                    self.showLoader = false;
+                    self.submitted = false;
                     self.isDataChanged = false;
+                    self.$toast.success("Item created successfully.", self.sharedService.toastNotificationConfig);
+                    console.log("Created item " + self.entity.id);
                     self.$router.push("/collections/items/details");
                 }).catch(error => {
                     self.showLoader = false;
@@ -128,15 +132,16 @@ export default class EntityService {
                         const errorMessages = self.sharedService.extractErrorMessage(error.response.data.validationErrors);
                         errorMessages.map(el => self.$toast.error(el, self.sharedService.toastNotificationConfig));
                     } else {
-                        self.$toast.error("Failed to add an Item", self.sharedService.toastNotificationConfig);
+                        self.$toast.error("Unable to create item. Please try again!", self.sharedService.toastNotificationConfig);
                     }
                 });
             } else {
                 self.itemService.updateItem(self.entity).then(success => {
                     self.showLoader = false;
-                    self.$toast.success("Item details updated successfully", self.sharedService.toastNotificationConfig);
                     self.submitted = false;
                     self.isDataChanged = false;
+                    console.log("Updated item " + self.entity.id);
+                    self.$toast.success("Item details updated successfully.", self.sharedService.toastNotificationConfig);
                 }).catch(error => {
                     self.showLoader = false;
                     self.submitted = false;
@@ -144,10 +149,41 @@ export default class EntityService {
                         const errorMessages = self.sharedService.extractErrorMessage(error.response.data.validationErrors);
                         errorMessages.map(el => self.$toast.error(el, self.sharedService.toastNotificationConfig));
                     } else {
-                        self.$toast.error("Item details update failed!", self.sharedService.toastNotificationConfig);
+                        self.$toast.error("Unable to update item details. Please try again!", self.sharedService.toastNotificationConfig);
                     }
                 });
             }
+        } else if (self.baseUrl === 'file') {          
+            // file Validation rules
+            if (!self.entity.name) {
+                self.$toast.error("Please provide required fields!", self.sharedService.toastNotificationConfig);
+                return false;
+            }
+            self.showLoader = true;
+            const payload = { name: self.entity.name, description: self.entity.description };
+            self.primaryFileService.updatePrimaryFile(self.entity.id, payload).then(response => {
+                self.showLoader = false;
+                self.submitted = false;
+                self.isDataChanged = false;
+                // TODO below is a workaround:
+                // save selectedFile into localStorage as sync doesn't seem to take effect on write
+                let vuex = JSON.parse(localStorage.getItem("vuex"));                    
+                localStorage.setItem("vuex", JSON.stringify({
+                    ...vuex,
+                    selectedFile: self.selectedFile,
+                }));                    
+                console.log("Updated file " + self.entity.id);
+                self.$toast.success("File details updated successfully.", self.sharedService.toastNotificationConfig);
+            }).catch(error => {
+                self.showLoader = false;
+                self.submitted = false;
+                if (error.response && error.response.data && error.response.data.validationErrors) {
+                    const errorMessages = self.sharedService.extractErrorMessage(error.response.data.validationErrors);
+                    errorMessages.map(el => self.$toast.error(el, self.sharedService.toastNotificationConfig));
+                } else {
+                    self.$toast.error("Unable to update file details. Please try again!", self.sharedService.toastNotificationConfig);
+                }
+            });
         }
     }
 
